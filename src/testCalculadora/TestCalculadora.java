a52b3129c51acb8a74a99eb5770b05916d9fbbb9
Propietario <Propietario@XANA>
2020-05-31 10:51:33 +0200

    Añadida clase para casos de pruebas con JUnit

---
+++ src/testCalculadora/TestCalculadora.java
@@ -0,0 +1,15 @@
+package testCalculadora;
+
+import calculadora.RPN;
+import junit.framework.TestCase;
+import org.junit.*;
+
+public class TestCalculadora   extends TestCase{
+	@Test
+	public void testCalculadora() {
+		RPN test01 = new RPN("4 5 * 7 2 / - 3 %");
+		assertEquals(1.5,test01.resultado( ));
+	}
+}
+
+
